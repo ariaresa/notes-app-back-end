a34f1797f901394168ff6cbca5b0a0dde398e1fd
ariaresa <ariaresa93@gmail.com>
2023-02-10 20:42:52 +0700

    fix handler updatedAt-2

--- src/handler.js
+++ src/handler.js
@@ -69,7 +69,7 @@ const editNoteByIdHandler = (request, h) => {
     const { id } = request.params;
 
     const { title, tags, body } = request.payload;
-    const { updatedAt } = new Date().toISOString;
+    const updatedAt = new Date().toISOString();
 
     const index = notes.findIndex((note) => note.id === id);
 
