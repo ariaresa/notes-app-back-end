d5d153f1d6582b3fb349ac26d037db5c35cdc40b
ariaresa <ariaresa93@gmail.com>
2023-09-16 17:49:20 +0700

    user registration implemented

---
+++ src/validator/users/schema.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-trailing-spaces */
+const Joi = require('joi');
+ 
+const UserPayloadSchema = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+  fullname: Joi.string().required(),
+});
+
+module.exports = UserPayloadSchema;
